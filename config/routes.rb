465577b253bde16e7bffe84ae0ed296581dfb0af
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-10-23 19:16:05 +0100

    Added_Statistics_Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :tsunamis
   resources :recordeds
   resources :deadliests
+  resources :statistics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
