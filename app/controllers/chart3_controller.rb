1d3f606b7175c054cb13751010ee43ce9d44c26e
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-10-26 19:01:06 +0100

    Chart3 fix

--- app/controllers/chart3_controller.rb
+++ app/controllers/chart3_controller.rb
@@ -1,7 +1,7 @@
 class Chart3Controller < ApplicationController
 	
 	def index
-		@tsunami_count = Tsunami.where("Total_deaths != 0").pluck(:Date, :Total_deaths)
+		@tsunami_count = Tsunami.where("Total_deaths != 0").pluck(:Date, :Total_Deaths)
 		@tsunami_count.each do |x|
 			x[0] = "#{x[0].day}/#{x[0].month}/#{x[0].year}"
 		end
