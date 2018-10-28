feca911d7af544caf3b10220ab843e5f2ea59788
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-10-28 21:45:11 +0000

    Deleted houses column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_141950) do
+ActiveRecord::Schema.define(version: 2018_10_24_141718) do
 
   create_table "deadly_quakes", force: :cascade do |t|
     t.string "EqCode"
