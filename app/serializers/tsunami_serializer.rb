d8fac82e6d656db3d3b33f5b811db2a7bf24877e
fenlen <ryuukiko@gmail.com>
2018-10-28 23:28:01 +0000

    expanded deadly quakes tables and details

--- app/serializers/tsunami_serializer.rb
+++ app/serializers/tsunami_serializer.rb
@@ -1,3 +1,3 @@
 class TsunamiSerializer < ActiveModel::Serializer
-  attributes :id, :Date, :Time, :SameDayAs, :Focal_Depth, :Primary_Magnitude, :Country, :Location_Name, :Latitude, :Longitude, :Maximun_Water_Height, :Total_Deaths, :Total_Injuries, :Total_Damage_Million_Dollars, :Total_Houses_Destroyed, :Total_Houses_Damaged
+  attributes :id, :Date, :Time, :SameDayAs, :Focal_Depth, :Primary_Magnitude, :Country, :Location_Name, :Latitude, :Longitude, :Maximum_Water_Height, :Total_Deaths, :Total_Injuries, :Total_Damage_Million_Dollars, :Total_Houses_Destroyed, :Total_Houses_Damaged
 end
