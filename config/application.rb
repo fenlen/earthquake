709956be8115b2b993867051b2ff8a8887ace4e4
fenlen <ryuukiko@gmail.com>
2018-10-29 13:00:44 +0000

    possible fix to heroku not loading scripts

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Earthquake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
     config.paths.add 'app/api', glob: '**/*.rb'
     config.autoload_paths += Dir['#{Rails.root}/app/api/*']
     # Settings in config/environments/* take precedence over those specified here.
